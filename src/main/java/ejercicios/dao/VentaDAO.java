b6cd735ad0e1566315d5b2d3258f444b4e9e3900
Marc Pellicer <marcpelli3@gmail.com>
2023-11-12 20:23:35 +0100

    Cambios

--- src/main/java/ejercicios/dao/VentaDAO.java
+++ src/main/java/ejercicios/dao/VentaDAO.java
@@ -6,6 +6,5 @@ import ejercicios.dto.Venta;
 
 public interface VentaDAO extends JpaRepository<Venta, Integer>{
 
-	//Venta findByCodigos(int codigo_cajero, int codigo_maquina, int codigo_producto);
 
 }
